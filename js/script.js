e1d281f3657339f809d3ddee74aa1f93f444b353
Taavi-Univer <taavi.univer@gmail.com>
2022-05-21 20:35:33 +0300

    checkBox

--- js/script.js
+++ js/script.js
@@ -1,36 +1,55 @@
-let views = document.getElementById("viewsCount");
+let views = document.getElementById("viewCount");
 let priceRange = document.getElementById("priceRange");
-let price = document.querySelector(".price");
-// console.log(views.textContent);
+let price = document.getElementById("price");
+let checkBox = document.getElementById("checkBox");
 
-priceRange.addEventListener("input", (e) => {
-  level = e.target.value;
-  // console.log(level);
-
-  if (level <= 20) {
+function printViewsAndPrices(value) {
+  if (value <= 20) {
     views.textContent = "10K";
-    price.textContent = "$8";
-  } else if (level <= 40) {
+    price.textContent = 8;
+  } else if (value <= 40) {
     views.textContent = "50K";
-    price.textContent = "$12";
-  } else if (level <= 60) {
+    price.textContent = 12;
+  } else if (value <= 60) {
     views.textContent = "100K";
-    price.textContent = "$16";
-  } else if (level <= 80) {
+    price.textContent = 16;
+  } else if (value <= 80) {
     views.textContent = "500K";
-    price.textContent = "$24";
+    price.textContent = 24;
   } else {
     views.textContent = "1M";
-    price.textContent = "$36";
+    price.textContent = 36;
   }
+}
+
+priceRange.addEventListener("input", (e) => {
+  printViewsAndPrices(e.target.value);
 });
 
 priceRange.oninput = function () {
-  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  let value = ((this.value - this.min) / (this.max - this.min)) * 100;
   this.style.background =
     "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) " +
     value +
     "%, #eaeefb " +
     value +
     "%, #eaeefb 100%)";
+
+  checkBox.checked = false;
 };
+
+checkBox.addEventListener("input", (e) => {
+  let value = parseInt(price.innerHTML);
+
+  if (checkBox.checked == true) {
+    console.log("discount");
+
+    let discount = value * 0.25;
+
+    price.textContent = value - discount;
+  } else {
+    console.log("no discount");
+
+    printViewsAndPrices(priceRange.value);
+  }
+});
